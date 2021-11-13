b9ca42c4964ee33dcb75be87ddc16137a20e62e9
MattiaMosso <91486653+MattiaMosso@users.noreply.github.com>
2021-11-13 09:38:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 
 int main() {
-   return 0;
+  int n=0, i=0,l=1,k=0;
+  cin >> n;
+  cout << 1 <<endl;
+
+  while (k<=n){
+
+      k=i+l;
+      if(k<=n){
+          cout << k <<endl;
+      }
+      i=l;
+      l=k;
+  }
+    return 0;
 }
